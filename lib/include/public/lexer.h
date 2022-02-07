e2ef7d849572bf948b5072142db4bdb50a9d68bd
Marco Pinto <marco.alp120@gmail.com>
2022-02-07 00:14:36 +0000

    Added fmt dependency with conan
    - Using conan as dependencies manager;
    - Added fmt lib to easily format strings;

--- lib/include/public/lexer.h
+++ lib/include/public/lexer.h
@@ -1,35 +1,36 @@
+#include <fmt/format.h>
+
 #include <string>
 #include <vector>
 
 #include "frit_export.h"
 
 namespace frit {
+
 enum class token_type { number, ident, add, sub, mul, div, mod, lparen, rparen };
 
-struct FRIT_EXPORT token {
+struct token {
 	token_type type;
 	std::string value;
 	inline bool operator==(const token& other) const { return type == other.type && value == other.value; }
 };
 
-class FRIT_EXPORT lexer {
+class lexer_exception : public std::exception {
 public:
-	std::vector<token> tokenize(const std::string& source);
-	std::vector<token> tokenize_file(const std::string& filepath);
-
-private:
-	char m_current_char{};
-	std::pair<int, int> m_position{1, 1};
-};
-
-class FRIT_EXPORT lexer_exception : public std::exception {
-public:
-	explicit lexer_exception(std::string message) : m_message(std::move(message)) {}
+	explicit lexer_exception(const std::string& message, const std::pair<int, int>& position)
+		: m_message(std::move(fmt::format("{} @{}:{}", message, position.first, position.second))) {}
 
 	inline const char* what() const noexcept override { return m_message.c_str(); }
 
 private:
 	std::string m_message{};
 };
+
+namespace lexer {
+
+std::vector<token> FRIT_EXPORT tokenize(const std::string& source);
+std::vector<token> FRIT_EXPORT tokenize_file(const std::string& filepath);
+
+}  // namespace lexer
 
 }  // namespace frit
--- lib/src/lexer.cpp
+++ lib/src/lexer.cpp
@@ -1,32 +1,32 @@
 #include "lexer.h"
 
 #include <fstream>
-#include <sstream>
 
 namespace frit {
+
 std::vector<token> lexer::tokenize(const std::string& source) {
 	std::vector<token> tokens;
+	char current_char;
+	std::pair<int, int> current_position{1, 1};
 
 	auto parse_number = [&](size_t& i) -> std::string {
-		std::string number{m_current_char};
+		std::string number{current_char};
 
 		for (size_t j = i + 1; j < source.size(); ++j) {
-			auto next = source.at(j);
+			char next = source.at(j);
 			// end of number
 			if (!std::isdigit(next) && next != '.') {
 				break;
 			}
 			// increment column position
-			++m_position.second;
+			++current_position.second;
 			// a number can only contain a single dot
 			if (next == '.' && number.find('.') != std::string::npos) {
-				std::stringstream error;
-				error << m_position.first << ":" << m_position.second;
-				throw lexer_exception("invalid char at " + error.str());
+				throw lexer_exception("invalid char", current_position);
 			}
 			// append to number string
-			m_current_char = next;
-			number += m_current_char;
+			current_char = next;
+			number += current_char;
 			++i;
 		}
 
@@ -34,34 +34,32 @@ std::vector<token> lexer::tokenize(const std::string& source) {
 	};
 
 	for (size_t i = 0; i < source.size(); ++i) {
-		m_current_char = source.at(i);
+		current_char = source.at(i);
 
-		if (m_current_char == ' ') {
-			tokens.push_back({token_type::ident, " "});
-		} else if (m_current_char == '\t') {
-			tokens.push_back({token_type::ident, "\t"});
-		} else if (m_current_char == '\n') {
-			tokens.push_back({token_type::ident, "\n"});
-			m_position = {1, 1};
-		} else if (m_current_char == '+') {
-			tokens.push_back({token_type::add, "+"});
-		} else if (m_current_char == '-') {
-			tokens.push_back({token_type::sub, "-"});
-		} else if (m_current_char == '*') {
-			tokens.push_back({token_type::mul, "*"});
-		} else if (m_current_char == '/') {
-			tokens.push_back({token_type::div, "/"});
-		} else if (m_current_char == '%') {
-			tokens.push_back({token_type::mod, "%"});
-		} else if (m_current_char == '(') {
-			tokens.push_back({token_type::lparen, "("});
-		} else if (m_current_char == ')') {
-			tokens.push_back({token_type::rparen, ")"});
-		} else if (std::isdigit(m_current_char)) {
+		if (current_char == ' ' || current_char == '\t') {
+			tokens.push_back({token_type::ident});
+		} else if (current_char == '\n') {
+			tokens.push_back({token_type::ident});
+			current_position = {1, 1};
+		} else if (current_char == '+') {
+			tokens.push_back({token_type::add});
+		} else if (current_char == '-') {
+			tokens.push_back({token_type::sub});
+		} else if (current_char == '*') {
+			tokens.push_back({token_type::mul});
+		} else if (current_char == '/') {
+			tokens.push_back({token_type::div});
+		} else if (current_char == '%') {
+			tokens.push_back({token_type::mod});
+		} else if (current_char == '(') {
+			tokens.push_back({token_type::lparen});
+		} else if (current_char == ')') {
+			tokens.push_back({token_type::rparen});
+		} else if (std::isdigit(current_char)) {
 			tokens.push_back({token_type::number, parse_number(i)});
 		}
 
-		++m_position.first;
+		++current_position.first;
 	}
 
 	return tokens;
@@ -70,7 +68,7 @@ std::vector<token> lexer::tokenize(const std::string& source) {
 std::vector<token> lexer::tokenize_file(const std::string& filepath) {
 	std::ifstream source{filepath};
 	if (!source.is_open()) {
-		throw lexer_exception("could not open file");
+		throw std::exception("could not open file");
 	}
 	return tokenize(std::string{std::istreambuf_iterator<char>{source}, {}});
 }
